660f36bdc6004a86a100dc3f7d70ca7e5a88fa5c
Mustafa Topal <mt8640695@gmail.com>
2024-12-15 23:18:45 +0300

    Memory Game
    Memory game in arrays

---
+++ HafizaOyunu/src/Kart.java
@@ -0,0 +1,25 @@
+public class Kart {
+
+        private char deger;
+        private boolean tahmin = false;
+
+        public Kart(char deger) {
+            this.deger = deger;
+        }
+
+        public char getDeger() {
+            return deger;
+        }
+
+        public boolean isTahmin() {
+            return tahmin;
+        }
+
+        public void setTahminEt(boolean tahmin) {
+            this.tahmin = tahmin;
+        }
+
+
+    }
+
+
